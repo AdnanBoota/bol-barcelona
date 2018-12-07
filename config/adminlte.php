6b773be08c1f337bd051fd388a673df226dcecc7
Adnan Boota <inshallah_1@yahoo.com>
2018-12-07 23:25:21 +0100

    menu updated

--- config/adminlte.php
+++ config/adminlte.php
@@ -30,9 +30,9 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>Bol</b>-Barcelona',
 
-    'logo_mini' => '<b>A</b>LT',
+    'logo_mini' => '<b>Bol</b>',
 
     /*
     |--------------------------------------------------------------------------
@@ -45,7 +45,7 @@ return [
     |
     */
 
-    'skin' => 'blue',
+    'skin' => 'black',
 
     /*
     |--------------------------------------------------------------------------
@@ -108,81 +108,113 @@ return [
     */
 
     'menu' => [
-        'MAIN NAVIGATION',
+        'Dasboard',
         [
-            'text' => 'Blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
-        ],
-        [
-            'text'        => 'Pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'file',
-            'label'       => 4,
+            'text' => 'Dashboard',
+            'url' => 'admin/pages',
+            'icon' => 'file',
+            'label' => 4,
             'label_color' => 'success',
         ],
-        'ACCOUNT SETTINGS',
+        'Orders Information',
         [
-            'text' => 'Profile',
-            'url'  => 'admin/settings',
-            'icon' => 'user',
+            'text' => 'Blog',
+            'url' => 'admin/blog',
+            'can' => 'manage-blog',
         ],
         [
-            'text' => 'Change Password',
-            'url'  => 'admin/settings',
-            'icon' => 'lock',
+            'text' => 'Orders',
+            'url' => 'admin/pages',
+            'icon' => 'file',
+            'label' => 4,
+            'label_color' => 'success',
         ],
         [
-            'text'    => 'Multilevel',
-            'icon'    => 'share',
+            'text' => 'Products',
+            'icon' => 'share',
             'submenu' => [
                 [
-                    'text' => 'Level One',
-                    'url'  => '#',
+                    'text' => 'All Products',
+                    'url' => '#',
                 ],
                 [
-                    'text'    => 'Level One',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'Level Two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'Level Two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'Level Three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'Level Three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'Level One',
-                    'url'  => '#',
+                    'text' => 'Create Product',
+                    'url' => '#',
                 ],
             ],
         ],
-        'LABELS',
         [
-            'text'       => 'Important',
-            'icon_color' => 'red',
+            'text' => 'Vouchers',
+            'icon' => 'share',
+            'submenu' => [
+                [
+                    'text' => 'All Vouchers',
+                    'url' => '#',
+                ],
+                [
+                    'text' => 'Create Voucher',
+                    'url' => '#',
+                ],
+            ],
         ],
         [
-            'text'       => 'Warning',
-            'icon_color' => 'yellow',
+            'text' => 'Bowls',
+            'icon' => 'share',
+            'submenu' => [
+                [
+                    'text' => 'All Bowls',
+                    'url' => '#',
+                ],
+                [
+                    'text' => 'Create bowl',
+                    'url' => '#',
+                ],
+            ],
         ],
         [
-            'text'       => 'Information',
-            'icon_color' => 'aqua',
+            'text' => 'Payment Methods',
+            'icon' => 'share',
+            'submenu' => [
+                [
+                    'text' => 'All Payments',
+                    'url' => '#',
+                ],
+                [
+                    'text' => 'Create Payment',
+                    'url' => '#',
+                ],
+            ],
         ],
+        'ACCOUNT SETTINGS',
+//        [
+//            'text' => 'Profile',
+//            'url' => 'admin/settings',
+//            'icon' => 'user',
+//        ],
+        [
+            'text' => 'Change Password',
+            'url' => 'admin/settings',
+            'icon' => 'lock',
+        ],
+        [
+            'text' => 'Logout',
+            'url' => 'admin/settings',
+            'icon' => 'lock',
+        ],
+
+//        'LABELS',
+//        [
+//            'text' => 'Important',
+//            'icon_color' => 'red',
+//        ],
+//        [
+//            'text' => 'Warning',
+//            'icon_color' => 'yellow',
+//        ],
+//        [
+//            'text' => 'Information',
+//            'icon_color' => 'aqua',
+//        ],
     ],
 
     /*
@@ -218,7 +250,7 @@ return [
 
     'plugins' => [
         'datatables' => true,
-        'select2'    => true,
-        'chartjs'    => true,
+        'select2' => true,
+        'chartjs' => true,
     ],
 ];
