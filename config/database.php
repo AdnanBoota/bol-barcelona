d9f3c37dca73e6f2857982f916eea4af3d212292
AdnanBoota <inshallah_1@yahoo.com>
2019-03-17 20:00:58 +0100

    all latest changes...

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'db4free.net'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'bolbarcelona'),
-            'username' => env('DB_USERNAME', 'bolbarcelona'),
-            'password' => env('DB_PASSWORD', 'bolbarcelona'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
